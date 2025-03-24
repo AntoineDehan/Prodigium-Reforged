40863e710652ef7d10b00cb4b8e1f2ddea6f051c
AntoineDehan <143590775+AntoineDehan@users.noreply.github.com>
2025-03-24 10:45:22 +0100

    Removed Netherite Battlemage, custom recipes for every necromancer armors + enderium smithing template removed from loot table

--- kubejs/server_scripts/removed_recipes.js
+++ kubejs/server_scripts/removed_recipes.js
@@ -1,4 +1,10 @@
 ServerEvents.recipes((event) => {
   // Misc
   event.remove({ output: "terrawhips_and_more:slime_crown" });
+
+  // Armor
+  event.remove({ output: "irons_spellbooks:netherite_magee_helmet" });
+  event.remove({ output: "irons_spellbooks:netherite_mage_chestplate" });
+  event.remove({ output: "irons_spellbooks:netherite_mage_leggings" });
+  event.remove({ output: "irons_spellbooks:netherite_mage_boots" });
 });
