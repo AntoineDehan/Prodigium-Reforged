7b55d4777a9337249267d2613ecf1892905ee1a0
AntoineDehan <143590775+AntoineDehan@users.noreply.github.com>
2025-07-21 10:59:30 +0200

    Fixed Mossy tooth being said to be in King Slime's Treasure bag and not Swampjaw. Added missing bosses to the loot scaling

--- kubejs/client_scripts/jei_tooltip.js
+++ kubejs/client_scripts/jei_tooltip.js
@@ -119,7 +119,7 @@ JEIEvents.information((event) => {
   // Swampjaw
   event.addItem(
     ["meetyourfight:mossy_tooth"],
-    ["Obtained in the King Slime's Treasure Bag"]
+    ["Obtained in the Swampjaw's Treasure Bag"]
   );
 
   // Conjurer
