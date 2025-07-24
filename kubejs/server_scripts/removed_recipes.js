815472b52e302eb466a39349ed06dec4c0336c41
AntoineDehan <143590775+AntoineDehan@users.noreply.github.com>
2025-07-24 19:54:16 +0200

    Removed Terrawhips mention in files Started to add the new bow system

--- kubejs/server_scripts/removed_recipes.js
+++ kubejs/server_scripts/removed_recipes.js
@@ -12,32 +12,6 @@ ServerEvents.recipes((event) => {
   event.remove({ output: "irons_spellbooks:netherite_mage_leggings" });
   event.remove({ output: "irons_spellbooks:netherite_mage_boots" });
 
-  // Stick issue kek
-  event.remove({ output: "simple_weapons:ironwood_dagger" });
-  event.remove({ output: "simple_weapons:ironwood_scythe" });
-  event.remove({ output: "simple_weapons:ironwood_sickle" });
-  event.remove({ output: "simple_weapons:ironwood_greatsword" });
-  event.remove({ output: "simple_weapons:ironwood_katana" });
-  event.remove({ output: "simple_weapons:ironwood_scimitar" });
-  event.remove({ output: "simple_weapons:knightmetal_dagger" });
-  event.remove({ output: "simple_weapons:knightmetal_scythe" });
-  event.remove({ output: "simple_weapons:knightmetal_sickle" });
-  event.remove({ output: "simple_weapons:knightmetal_fist" });
-  event.remove({ output: "simple_weapons:knightmetal_spear" });
-  event.remove({ output: "simple_weapons:knightmetal_greatsword" });
-  event.remove({ output: "simple_weapons:knightmetal_katana" });
-  event.remove({ output: "simple_weapons:knightmetal_scimitar" });
-  event.remove({ output: "simple_weapons:steeleaf_dagger" });
-  event.remove({ output: "simple_weapons:steeleaf_fist" });
-  event.remove({ output: "simple_weapons:steeleaf_scythe" });
-  event.remove({ output: "simple_weapons:steeleaf_sickle" });
-  event.remove({ output: "simple_weapons:steeleaf_spear" });
-  event.remove({ output: "simple_weapons:steeleaf_greatsword" });
-  event.remove({ output: "simple_weapons:steeleaf_katana" });
-  event.remove({ output: "simple_weapons:steeleaf_scimitar" });
-  event.remove({ output: "simple_weapons:ironwood_fist" });
-  event.remove({ output: "simple_weapons:ironwood_spear" });
-
   /// NPC Uncraftable items
 
   // Guns
@@ -47,4 +21,7 @@ ServerEvents.recipes((event) => {
 
   // Tom's Simple Storage
   event.remove({ mod: "toms_storage" });
+
+  // Archery Expansion
+  event.remove({ mod: "archeryexp" });
 });
