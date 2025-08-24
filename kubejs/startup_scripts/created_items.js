33d07dcec758ae6608e7a0d53d8c5998e6205c49
AntoineDehan <143590775+AntoineDehan@users.noreply.github.com>
2025-08-24 13:25:08 +0200

    Reworked boss summon items (now you hold if for a few seconds and it spawn the boss a bit further)

--- kubejs/startup_scripts/created_items.js
+++ kubejs/startup_scripts/created_items.js
@@ -1,9 +1,24 @@
 StartupEvents.registry("item", (event) => {
+  // Boss summons
   event
     .create("prodigium_reforged:slime_crown")
+    .displayName("Slime Crown")
     .texture("prodigium_reforged:item/slime_crown")
-    .useDuration((itemstack) => 60)
-    .useAnimation("bow");
+    .use((level, player, hand) => true)
+    .useDuration(() => 40)
+    .useAnimation("bow")
+    .finishUsing((itemstack, level, entity) => {
+      if (!entity.player) return itemstack;
+
+      let x = Math.floor(entity.x) + 10;
+      let y = Math.floor(entity.y);
+      let z = Math.floor(entity.z) + 8;
+
+      level.runCommandSilent(`summon terra_entity:king_slime ${x} ${y} ${z}`);
+
+      itemstack.shrink(1);
+      return itemstack;
+    });
 
   event
     .create("prodigium_reforged:dark_armor_scrap")
