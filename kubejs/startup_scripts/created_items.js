9b116bd2b6f01eebcfe7ff68caadf0cd4e94c895
AntoineDehan <143590775+AntoineDehan@users.noreply.github.com>
2025-08-27 10:34:02 +0200

    Added 3 new boss summoning items (some placeholder textures)

--- kubejs/startup_scripts/created_items.js
+++ kubejs/startup_scripts/created_items.js
@@ -1,4 +1,12 @@
 StartupEvents.registry("item", (event) => {
+  // Materials
+  event
+    .create("prodigium_reforged:bee_wax")
+    .texture("prodigium_reforged:item/bee_wax");
+  event
+    .create("prodigium_reforged:dark_armor_scrap")
+    .texture("prodigium_reforged:item/dark_armor_scrap");
+
   //// Boss summons
 
   // King Slime
@@ -22,7 +30,51 @@ StartupEvents.registry("item", (event) => {
       return itemstack;
     });
 
-  // Brain of Chtulhu
+  // Eye Of Cthulhu
+  event
+    .create("prodigium_reforged:suspicious_looking_eye")
+    .displayName("Suspicious Looking Eye")
+    .use((level, player, hand) => true)
+    .useDuration(() => 40)
+    .useAnimation("bow")
+    .finishUsing((itemstack, level, entity) => {
+      if (!entity.player) return itemstack;
+
+      let x = Math.floor(entity.x) + 10;
+      let y = Math.floor(entity.y) + 3;
+      let z = Math.floor(entity.z) + 8;
+
+      level.runCommandSilent(
+        `summon terra_entity:eye_of_cthulhu ${x} ${y} ${z}`
+      );
+
+      itemstack.shrink(1);
+      return itemstack;
+    });
+
+  // Eater of Worlds
+  event
+    .create("prodigium_reforged:worm_food")
+    .displayName("Worm Food")
+    .use((level, player, hand) => true)
+    .useDuration(() => 40)
+    .useAnimation("bow")
+    .finishUsing((itemstack, level, entity) => {
+      if (!entity.player) return itemstack;
+
+      let x = Math.floor(entity.x) + 10;
+      let y = Math.floor(entity.y);
+      let z = Math.floor(entity.z) + 8;
+
+      level.runCommandSilent(
+        `summon terra_entity:eater_of_worlds ${x} ${y} ${z}`
+      );
+
+      itemstack.shrink(1);
+      return itemstack;
+    });
+
+  // Brain of Cthulhu
   event
     .create("prodigium_reforged:bloody_spine")
     .texture("prodigium_reforged:item/bloody_spine")
@@ -37,14 +89,10 @@ StartupEvents.registry("item", (event) => {
       let z = Math.floor(entity.z) + 8;
 
       level.runCommandSilent(
-        `summon terra_entity:brain_of_chtulhu ${x} ${y} ${z}`
+        `summon terra_entity:brain_of_cthulhu ${x} ${y} ${z}`
       );
 
       itemstack.shrink(1);
       return itemstack;
     });
-
-  event
-    .create("prodigium_reforged:dark_armor_scrap")
-    .texture("prodigium_reforged:item/dark_armor_scrap");
 });
