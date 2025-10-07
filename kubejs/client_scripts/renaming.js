20da325b09bfce92dce319478bff625ad69e7206
AntoineDehan <antoine.dehandartial@gmail.com>
2025-10-07 18:12:48 +0200

    feat(charms): renamed certain curio items and custom recipes

---
+++ kubejs/client_scripts/renaming.js
@@ -0,0 +1,5 @@
+ClientEvents.lang("en_us", (event) => {
+  event.renameItem("sweet_charm_o_mine:copper_pocket", "Iron Bullet Pocket");
+  event.renameItem("sweet_charm_o_mine:iron_pocket", "Cobalt Bullet Pocket");
+  event.renameItem("sweet_charm_o_mine:gold_pocket", "Palladin Bullet Pocket");
+});
