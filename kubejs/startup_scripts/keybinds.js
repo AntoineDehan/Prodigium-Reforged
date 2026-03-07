78cd12f9f1a6e70de504df2be06c409c6206fa15
AntoineDehan <antoine.dehandartial@gmail.com>
2026-03-07 01:47:14 +0100

    feat(keybinds): first version of new keybinds

---
+++ kubejs/startup_scripts/keybinds.js
@@ -0,0 +1,16 @@
+KeyBindEvents.modify((event) => {
+  event.modifyKey("key.simplyskills.ability1", GLFW.GLFW_KEY_F);
+  event.modifyKey("key.bestiary.open", GLFW.GLFW_KEY_H);
+  event.modifyKey("key.puffish_skills.open", GLFW.GLFW_KEY_I);
+
+  event.remove("key.swapOffhand");
+  event.remove("desc.seasonhud.keybind.options");
+  event.remove("key.simplyskills.ability2");
+  event.remove("key.block_factorys_bosses.dodge_roll");
+  event.remove("dropoff.key.dump");
+  event.remove("dropoff.key.deposit");
+
+  event.addHideKey("key.block_factorys_bosses.dodge_roll");
+  event.addHideKey("dropoff.key.dump");
+  event.addHideKey("dropoff.key.deposit");
+});
