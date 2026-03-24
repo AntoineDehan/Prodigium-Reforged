2a5d7a9b79e0c3deaf67cb3a48c633e91aae5ea5
AntoineDehan <antoine.dehandartial@gmail.com>
2026-03-24 21:02:45 +0100

    fix(keybinds): switch offhand keybind is back

--- kubejs/startup_scripts/keybinds.js
+++ kubejs/startup_scripts/keybinds.js
@@ -2,13 +2,14 @@ KeyBindEvents.modify((event) => {
   event.modifyKey("key.simplyskills.ability1", GLFW.GLFW_KEY_F);
   event.modifyKey("key.bestiary.open", GLFW.GLFW_KEY_H);
   event.modifyKey("key.puffish_skills.open", GLFW.GLFW_KEY_I);
+  event.modifyKey("key.swapOffhand", GLFW.GLFW_KEY_L);
 
-  event.remove("key.swapOffhand");
   event.remove("desc.seasonhud.keybind.options");
   event.remove("key.simplyskills.ability2");
   event.remove("key.block_factorys_bosses.dodge_roll");
   event.remove("dropoff.key.dump");
   event.remove("dropoff.key.deposit");
+  event.remove("key.advancements");
 
   event.addHideKey("key.block_factorys_bosses.dodge_roll");
   event.addHideKey("dropoff.key.dump");
