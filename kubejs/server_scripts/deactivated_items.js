6bf7fa9a693b2409db6b36bb350bc13b49cf5de6
AntoineDehan <antoine.dehandartial@gmail.com>
2026-04-12 00:31:32 +0200

    fix(balancing): nerfed Charm of Fury

--- kubejs/server_scripts/deactivated_items.js
+++ kubejs/server_scripts/deactivated_items.js
@@ -2,6 +2,12 @@ ItemEvents.rightClicked((event) => {
   const player = event.player;
   const item = event.item;
 
+  if (item.id === "born_in_chaos_v1:charmof_fury") {
+    event.server.scheduleInTicks(1, (callback) => {
+      player.getCooldowns().addCooldown("born_in_chaos_v1:charmof_fury", 3200);
+    });
+  }
+
   const books = [
     "companions:book_ice_shard",
     "companions:book_ice_tornado",
