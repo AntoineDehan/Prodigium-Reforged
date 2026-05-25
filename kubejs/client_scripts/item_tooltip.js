ea8f6f2f66c4cf112d79fb833086b1edf40f53ec
AntoineDehan <antoine.dehandartial@gmail.com>
2026-05-25 12:06:14 +0200

    feat(items): added tooltips to some items

--- kubejs/client_scripts/item_tooltip.js
+++ kubejs/client_scripts/item_tooltip.js
@@ -22,6 +22,52 @@ ItemEvents.tooltip((event) => {
     Text.gold("Can be used to reforge any item"),
   ]);
 
+  event.add(
+    [
+      "numismatic-overhaul:bronze_coin",
+      "numismatic-overhaul:silver_coin",
+      "numismatic-overhaul:gold_coin",
+    ],
+    [Text.gray("Right Click while in Main Hand to add it in your purse!")],
+  );
+
+  event.add(
+    [
+      "endrem:black_eye",
+      "endrem:cold_eye",
+      "endrem:corrupted_eye",
+      "endrem:lost_eye",
+      "endrem:nether_eye",
+      "endrem:old_eye",
+      "endrem:rogue_eye",
+      "endrem:cursed_eye",
+      "endrem:evil_eye",
+      "endrem:guardian_eye",
+      "endrem:magical_eye",
+      "endrem:wither_eye",
+      "endrem:witch_eye",
+      "endrem:undead_eye",
+      "endrem:exotic_eye",
+      "endrem:cryptic_eye",
+    ],
+    [Text.gray("Used to open the Ender Portal")],
+  );
+
+  event.add(
+    [
+      "companions:book_ice_shard",
+      "companions:book_ice_tornado",
+      "companions:book_fire_mark",
+      "companions:book_brace",
+      "companions:book_heal_ring",
+      "companions:book_stone_spikes",
+      "companions:book_magic_ray",
+      "companions:book_black_hole",
+      "companions:book_naginita",
+    ],
+    [Text.gray("Only usable by pets!")],
+  );
+
   event.add("kybejs:ancient_cobalt_bow", [Text.gold("Infinite Arrows")]);
 
   // Bows
